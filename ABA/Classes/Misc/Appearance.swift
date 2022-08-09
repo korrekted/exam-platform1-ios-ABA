6bb94fbffe045ba5689ed09a67e3fd438734a767
akonst17 <akonst17@gmail.com>
2022-08-09 16:43:21 +0300

    обновления Nursing; build 3

--- ABA/Classes/Misc/Appearance.swift
+++ ABA/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 247, green: 250, blue: 252)
-    
     static let mainColor = UIColor(integralRed: 11, green: 119, blue: 189)
     static let mainColorAlpha = UIColor(integralRed: 11, green: 119, blue: 189, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 0, green: 173, blue: 239)
-    
-    static let progress1Color = UIColor(integralRed: 11, green: 119, blue: 189)
-    static let progress2Color = UIColor(integralRed: 0, green: 173, blue: 239)
-    static let progress3Color = UIColor(integralRed: 251, green: 217, blue: 2)
-    
+    static let secondColor = UIColor(integralRed: 0, green: 173, blue: 239)
+    static let thirdColor = UIColor(integralRed: 251, green: 217, blue: 2)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 247, green: 250, blue: 252)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
--- ABA/Classes/Misc/GlobalDefinitions.swift
+++ ABA/Classes/Misc/GlobalDefinitions.swift
@@ -17,6 +17,7 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
+    static let appStoreUrl = "https://apps.apple.com/app/aba-exam/id1623173587"
     static let appleAppID = "1623173587"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
