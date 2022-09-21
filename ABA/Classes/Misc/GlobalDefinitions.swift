3d21a85d77df2a041d0bc86c786d2e2778f1c2e6
akonst17 <akonst17@gmail.com>
2022-09-21 18:05:26 +0300

    интеграция sdk 2.0; новые урлы; build 7

--- ABA/Classes/Misc/GlobalDefinitions.swift
+++ ABA/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams2.quantresear.ch"
+    static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "ZbA7uBCXiTTI661K"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "K9LbiheAmbgNIIjn"
 
     static let applicationTag = "EXAM_ABA"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "K9LbiheAmbgNIIjn"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "cSHJiHDNNCuH1iYX"
 
     static let appStoreUrl = "https://apps.apple.com/app/aba-exam/id1623173587"
     static let appleAppID = "1623173587"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
